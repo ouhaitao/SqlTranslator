33e57bdce5091f80f99f06d971bcc70fb89f9c21
parry <parry@thecover.co>
2024-03-28 09:26:35 +0800

    fix(pageHelper): 新增方言自动映射，dm使用MySqlDialect

--- src/main/java/cn/cover/support/MybatisTranslateInterceptorAutoConfiguration.java
+++ src/main/java/cn/cover/support/MybatisTranslateInterceptorAutoConfiguration.java
@@ -1,8 +1,11 @@
 package cn.cover.support;
 
 import cn.cover.util.CollectionUtils;
+import com.github.pagehelper.dialect.helper.MySqlDialect;
+import com.github.pagehelper.page.PageAutoDialect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -32,5 +35,14 @@ public class MybatisTranslateInterceptorAutoConfiguration {
         }
         SqlTranslator sqlTranslator = builder.build();
         return new MybatisTranslateInterceptor(sqlTranslator);
+    }
+    
+    /**
+     * PageHelper的自动方言对象
+     * pageHelper如果没有配置方言，则会根据jdbc的url获取到对应的方言对象，这里达梦使用Mysql的方言
+     */
+    @ConditionalOnClass(name = "com.github.pagehelper.page.PageAutoDialect")
+    public void pageHelper() {
+        PageAutoDialect.registerDialectAlias("dm", MySqlDialect.class);
     }
 }
