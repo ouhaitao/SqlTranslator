495cb5a190703f3a1e1d3ba76493b493e53823a4
parry <parry@thecover.co>
2024-03-28 11:38:22 +0800

    feat(MybatisInterceptor): 创建拦截器时自动将拦截器注入mybatis

--- README.md
+++ README.md
@@ -10,7 +10,8 @@
 访问语法树是以`访问者`的形式，在适配语法的开发中，只需要新增一个`语法树节点类型`然后实现对应的访问方法即可。
 
 # 程序接入注意事项
-1. SQL转译的Myabtis拦截器应该放在Pagehelper的拦截器后面执行。
+1. 如果是项目中手动添加的PagHelper拦截器，则需要先添加sqlTranslator拦截器，以确保sqlTranslator拦截器后于PageHelper拦截器执行，
+如果使用的是PageHelper-spring-boot-stater，则sqlTranslator在创建拦截器时就会将其添加到mybatis。
 2. 达梦SQL中的表名、字段等，如果没有使用引号，则在执行时会默认转换成大写。并且达梦是大小写敏感数据库，所以如果表字段是小写，但是SQL没有使用引号则会报错找不到字段，所以转译拦截器会默认将表名、表字段转换成大写。在新建表、字段时，应该全部大写。
 3. PageHelper与sqlTranslator同样使用了jsqlparser，sqlTranslator中使用的是4.4版本，现在项目中使用的PageHelper是5.1，PageHelper-spring-boot-stater是1.2.3，他们使用的都是1.0的jsqlparser，会报错找不到方法。 所以需要pageHelper升级到5.3.3，PageHelper-spring-boot-stater升级到1.4.7。
 4. PageHelper的方言不设置或者使用PageHelper方言，只有PageHelper方言才会生效。PageHelper会根据jdbc的url判断使用合种方言，我们的sql是mysql语法，jdbc是dm等其他数据库，所以需要向PageAutoDialect添加方言映射，在sqlTranslator创建翻译拦截器时会向PageAutoDialect添加dm与MysqlDialect映射。
--- src/main/java/cn/cover/support/MybatisTranslateInterceptorAutoConfiguration.java
+++ src/main/java/cn/cover/support/MybatisTranslateInterceptorAutoConfiguration.java
@@ -3,12 +3,22 @@ package cn.cover.support;
 import cn.cover.util.CollectionUtils;
 import com.github.pagehelper.dialect.helper.MySqlDialect;
 import com.github.pagehelper.page.PageAutoDialect;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.Ordered;
+
+import java.util.List;
 
 /**
  * @author parry 2024/03/26
@@ -16,15 +26,21 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableConfigurationProperties(SqlTranslatorProperties.class)
-@ConditionalOnProperty(prefix = "sql.translator", value = {"origin-database", "target-database"})
 public class MybatisTranslateInterceptorAutoConfiguration {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     
     public static final String MYBATIS_INTERCEPTOR_BEAN_NAME = "sqlTranslatorMybatisInterceptor";
-
+    
+    /**
+     * PageHelper的拦截器是在afterPropertiesSet方法中添加
+     * 所以MybatisTranslateInterceptor会先于PageHelper的拦截器被添加进去，所以会在PageHelper后面执行
+     */
     @Bean(MYBATIS_INTERCEPTOR_BEAN_NAME)
-    public MybatisTranslateInterceptor mybatisInterceptor(SqlTranslatorProperties properties) {
+    @ConditionalOnProperty(prefix = "sql.translator", value = {"origin-database", "target-database"})
+    @ConditionalOnBean(SqlSessionFactory.class)
+    @Lazy(false)
+    public MybatisTranslateInterceptor mybatisInterceptor(SqlTranslatorProperties properties, List<SqlSessionFactory> sqlSessionFactoryList) {
         logger.info("创建MybatisTranslateInterceptor, properties={}", properties.toString());
         SqlTranslator.Builder builder = SqlTranslator.builder()
             .originDatabase(properties.getOriginDatabase())
@@ -34,7 +50,19 @@ public class MybatisTranslateInterceptorAutoConfiguration {
         }
         SqlTranslator sqlTranslator = builder.build();
         initPageHelperDialect();
-        return new MybatisTranslateInterceptor(sqlTranslator);
+        MybatisTranslateInterceptor mybatisTranslateInterceptor = new MybatisTranslateInterceptor(sqlTranslator);
+        sqlSessionFactoryList.stream()
+            .map(SqlSessionFactory::getConfiguration)
+            .filter(configuration -> {
+                List<Interceptor> interceptorList = configuration.getInterceptors();
+                if (CollectionUtils.isEmpty(interceptorList)) {
+                    return true;
+                }
+                return interceptorList.stream()
+                    .anyMatch(interceptor -> !MybatisTranslateInterceptor.class.isAssignableFrom(interceptor.getClass()));
+            })
+            .forEach(configuration -> configuration.addInterceptor(mybatisTranslateInterceptor));
+        return mybatisTranslateInterceptor;
     }
     
     /**
