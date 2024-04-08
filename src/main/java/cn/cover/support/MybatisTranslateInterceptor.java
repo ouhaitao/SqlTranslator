a59ca05c78e14f25dbfaf65808f0b40ed0037ff4
parry <parry@thecover.co>
2024-04-08 14:05:05 +0800

    style(mybatisInterceptor): 删除日志

--- src/main/java/cn/cover/support/MybatisTranslateInterceptor.java
+++ src/main/java/cn/cover/support/MybatisTranslateInterceptor.java
@@ -55,9 +55,7 @@ public class MybatisTranslateInterceptor implements Interceptor {
         } else {
             originBoundSql = statement.getBoundSql(parameter);
         }
-        long start = System.currentTimeMillis();
         String translateSql = sqlTranslator.translate(originBoundSql.getSql(), statement.getId());
-        long translateSqlTime = System.currentTimeMillis();
         if (logger.isDebugEnabled()) {
             logger.debug("原始SQL:{} 转译后SQL:{}", originBoundSql.getSql(), translateSql);
         }
@@ -70,8 +68,6 @@ public class MybatisTranslateInterceptor implements Interceptor {
             MappedStatement newMappedStatement = newMappedStatement(statement, originBoundSql, translateSql);
             invocationArgs[0] = newMappedStatement;
         }
-        long updateInvocationArgsTime = System.currentTimeMillis();
-        logger.info("转译SQL耗时:{} 动态修改mybatis对象耗时:{}", translateSqlTime - start, updateInvocationArgsTime - translateSqlTime);
         return invocation.proceed();
     }
     
